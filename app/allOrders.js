8e494c17bbde2091aaf7fd5d60f325557e059c1a
Corey Davenport <cairnelan@gmail.com>
2016-02-26 00:28:40 -0500

    Displaying data in allorders

--- app/allOrders.js
+++ app/allOrders.js
@@ -10,11 +10,68 @@ var {
   Component
 } = React;
 
-class allOrders extends Component {
+var myFirebaseRef = new Firebase("https://geminiapp.firebaseio.com/units");
+
+
+
+var allOrders = React.createClass( {
+  
+  getInitialState() {
+
+      return{
+      units: [],
+      unitData: null,
+    };
+  },
+
+  backButton () {
+    this.props.navigator.pop();
+  },
+
+  handleResponse(response){
+    this.setState({unitData: response});
+
+    console.log(this.state.unitData.sku);
+
+  },
+
+  dataView(){
+    if(this.state.unitData){
+    return(
+      <View>
+        <Text style={styles.unitText}>
+        Completed:{this.state.unitData.completed.sku}
+        </Text>
+
+        <Text  style={styles.unitText}>
+        Pending: {this.state.unitData.pending.sku}
+        </Text>
+      </View>
+      )
+    }else{
+      alert('Your Data is coming');
+    }
+  },
+
   render () {
+
+      var url = "https://geminiapp.firebaseio.com/units.json";
+      fetch(url)
+        .then((response) => response.json())
+        .then((response) => this.handleResponse(response))
+        .catch(error =>
+          this.setState({
+            isloading: false,
+            message: 'error on request' + error
+        }));
+    //console.log(myFirebaseRef + " Firebase connected");
+ 
+  
+
 		return (
 			<View style={styles.container}>
 				<Text>All Orders</Text>
+        {this.dataView()}
         <TouchableHighlight onPress={()=> this.backButton()} style={styles.backButton}>
           <Text style={styles.buttonText}>Go Back</Text>
         </TouchableHighlight>
@@ -22,10 +79,8 @@ class allOrders extends Component {
 		);
 	}
 
-  backButton () {
-    this.props.navigator.pop();
-  }
-}
+  
+});
 
 const styles = StyleSheet.create ({
 	container: {
@@ -36,9 +91,14 @@ const styles = StyleSheet.create ({
     padding: 10
 
 	},
+  unitText:{
+    fontSize: 22,
+    fontWeight: '400',
+    marginTop:10,
+  },
   backButton: {
   	height: 40,
-    alignSelf: 'flex-start',
+    alignSelf: 'center',
   	justifyContent: 'center',
   	backgroundColor: '#40B0FF'
   },
