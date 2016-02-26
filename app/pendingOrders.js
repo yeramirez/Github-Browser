3b468e378bcc0c11323c7c4b591867d44be9628b
Corey Davenport <cairnelan@gmail.com>
2016-02-26 14:56:06 -0500

    Data display for Pending Orders

--- app/pendingOrders.js
+++ app/pendingOrders.js
@@ -11,11 +11,56 @@ var {
 
 } = React;
 
-class pendingOrder extends Component {
+var pendingOrder = React.createClass ({
+  getInitialState() {
+
+      return{
+      units: [],
+      unitData: null,
+    };
+  },
+
+  backButton () {
+  this.props.navigator.pop();
+  },
+
+  handleResponse(response){
+    this.setState({unitData: response});
+
+    console.log(this.state.unitData.sku);
+
+  },
+
+  dataView(){
+    if(this.state.unitData){
+    return(
+      <View>
+        <Text  style={styles.unitText}>
+        Pending: {this.state.unitData.pending.sku}
+        </Text>
+      </View>
+      )
+    }else{
+      alert('Your Data is coming');
+    }
+  },
+
   render () {
+
+  var url = "https://geminiapp.firebaseio.com/units.json";
+  fetch(url)
+  .then((response) => response.json())
+  .then((response) => this.handleResponse(response))
+  .catch(error =>
+    this.setState({
+      isloading: false,
+      message: 'error on request' + error
+  }));
+
 		return (
 			<View style={styles.container}>
 				<Text>Pending Orders</Text>
+        {this.dataView()}
         <TouchableHighlight onPress={()=> this.backButton()} style={styles.backButton}>
           <Text style={styles.buttonText}>Go Back</Text>
         </TouchableHighlight>
@@ -24,10 +69,7 @@ class pendingOrder extends Component {
 		);
 	}
 
-  backButton () {
-    this.props.navigator.pop();
-  }
-}
+});
 
 const styles = StyleSheet.create ({
 	container: {
